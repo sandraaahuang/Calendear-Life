86ef89162e939cdf294dec55ea3c8245c76236a6
Sandra Huang <sandraaahuang@gmail.com>
2019-08-28 23:14:23 +0800

    finish reminders layout but only add mock data in it, also waiting for firebase data

--- app/src/main/java/com/sandra/calendearlife/home/HomeFragment.kt
+++ app/src/main/java/com/sandra/calendearlife/home/HomeFragment.kt
@@ -2,16 +2,15 @@ package com.sandra.calendearlife.home
 
 
 import android.os.Bundle
+import android.provider.CalendarContract
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProviders
-import com.sandra.calendearlife.R
 import com.sandra.calendearlife.data.Countdown
+import com.sandra.calendearlife.data.Reminders
 import com.sandra.calendearlife.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
@@ -22,7 +21,8 @@ class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        val adapter = HomeCountdownAdapter()
+        val countdownAdapter = HomeCountdownAdapter()
+        val remindersAdapter = HomeRemindersAdapter()
         val binding = HomeFragmentBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this
         viewModel
@@ -32,9 +32,15 @@ class HomeFragment : Fragment() {
         mockData.add(Countdown("88", "倒數數起來^^", "20200101"))
         mockData.add(Countdown("99", "倒數數起來:D", "20210101"))
 
-        binding.countdownRecyclerView.adapter = adapter
+        val mockdata2 = ArrayList<Reminders>()
+        mockdata2.add(Reminders("please remind me!!!", "20201010", false, false))
+        mockdata2.add(Reminders("who am I!!!", "20201012", false, false))
 
-        adapter.submitList(mockData)
+        binding.countdownRecyclerView.adapter = countdownAdapter
+        binding.remindersRecyclerView.adapter = remindersAdapter
+
+        countdownAdapter.submitList(mockData)
+        remindersAdapter.submitList(mockdata2)
 
 
         Log.d("sandraaa","mockData = $mockData")
