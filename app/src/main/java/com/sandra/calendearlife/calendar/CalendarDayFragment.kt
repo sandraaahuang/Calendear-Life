055d0a238d6c9b698d538ab4c3a9d483cfad200d
Sandra Huang <sandraaahuang@gmail.com>
2019-08-29 12:11:48 +0800

    finish add new reminders event layout

--- app/src/main/java/com/sandra/calendearlife/calendar/CalendarDayFragment.kt
+++ app/src/main/java/com/sandra/calendearlife/calendar/CalendarDayFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.sandra.calendearlife.NavigationDirections
 import com.sandra.calendearlife.databinding.CalendarDayFragmentBinding
 
 class CalendarDayFragment : Fragment() {
@@ -12,6 +14,10 @@ class CalendarDayFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val binding = CalendarDayFragmentBinding.inflate(inflater, container, false)
+
+        binding.hihi.setOnClickListener {
+            findNavController().navigate(NavigationDirections.actionGlobalRemindersFragment())
+        }
 
         return binding.root
     }
--- app/src/main/java/com/sandra/calendearlife/reminders/RemindersFragment.kt
+++ app/src/main/java/com/sandra/calendearlife/reminders/RemindersFragment.kt
@@ -1,2 +1,35 @@
 package com.sandra.calendearlife.reminders
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.sandra.calendearlife.data.Reminders
+import com.sandra.calendearlife.databinding.RemindersFragmentBinding
+
+class RemindersFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val binding = RemindersFragmentBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+
+        val addRemindersAdapter = AddRemindersAdapter()
+
+        val mockdata2 = ArrayList<Reminders>()
+        mockdata2.add(Reminders("please remind me!!!", "20201010", false, false))
+        mockdata2.add(Reminders("who am I!!!", "20201012", false, false))
+
+        binding.addRemindersRecyclerView.adapter = addRemindersAdapter
+        addRemindersAdapter.submitList(mockdata2)
+        addRemindersAdapter.notifyDataSetChanged()
+
+
+
+
+        return binding.root
+    }
+}
+
+
