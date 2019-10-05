31e5e6b324c3a6576a42bd3d65620b5c235deb69
Sandra Huang <sandraaahuang@gmail.com>
2019-10-05 15:12:29 +0800

    add isLogged value in UserManager to determine if user is Logged in

--- app/src/main/java/com/sandra/calendearlife/util/UserManager.kt
+++ app/src/main/java/com/sandra/calendearlife/util/UserManager.kt
@@ -48,4 +48,7 @@ object UserManager {
         set(value) {
             field = preferences.edit().putString(USERPHOTO, value)?.apply().toString()
         }
+
+    val isLoggedIn: Boolean
+        get() = id != null
 }
